e713cee02325ba0d5a5c94efa9f9d658e2b624a5
latermonk <iback.china@gmail.com>
2021-08-06 14:11:11 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+<!--    <img alt="Vue logo" src="./assets/logo.png">-->
 <!--    <Playboy></Playboy>-->
 <!--    <HelloWorld msg="Welcome to Your Vue.js App"/>-->
     <HelloWorld></HelloWorld>
